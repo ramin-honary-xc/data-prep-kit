ddd1618932fcf4d64e0ed297ead65d7c4279f50a
Ramin Honary <ramin.honary@cross-compass.com>
2023-08-26 02:36:38 +0900

    create image mask kit

---
+++ imgmaskkit.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+import DataPrepKit.ImageCropperGUI as gui
+
+import sys
+
+import PyQt5.QtWidgets as qt
+
+####################################################################################################
+
+def main():
+    app = qt.QApplication(sys.argv)
+    app_window = gui.ImgMaskGUI()
+    app_window.show()
+    sys.exit(app.exec_())
+
+####################################################################################################
+
+if __name__ == '__main__':
+    main()
