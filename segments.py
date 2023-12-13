600c28e467d2954449e21143d41e56a3b4c6c48c
Ramin Honary <ramin.honary@cross-compass.com>
2023-12-13 21:58:53 +0900

    create example script

--- segments.py
+++ segments.py
@@ -4,10 +4,27 @@ import math
 from pathlib import Path
 import DataPrepKit.ORBMatcher as orb
 from DataPrepKit.CachedCVImageLoader import CachedCVImageLoader
+from DataPrepKit.SingleFeatureMultiCrop import SingleFeatureMultiCrop
 
+usage = """
+  Modify these variables to point to the two input images: the path to
+  the reference image, which is searched for in the target image, and
+  the path to the target image. Also provide a directory to which
+  resulting matches can be saved.
+  """
 chips_image_path = Path('/home/ramin/datasets/crop-tool-testing/chips/chips-black-bg.png')
-chip_ref_path    = Path('/home/ramin/datasets/crop-tool-testing/chips/chips-pattern.png')
+chips_ref_path   = Path('/home/ramin/datasets/crop-tool-testing/chips/chips-pattern.png')
 output_dir_path  = Path('/home/ramin/Desktop/results')
+
+#---------------------------------------------------------------------------------------------------
+
+def check_paths():
+    """Check that the input images exist."""
+    return \
+        chips_image_path.exists() and \
+        chips_ref_path.exists() 
+
+#---------------------------------------------------------------------------------------------------
 
 def dice_image(imgpath, outdirpath):
     imgpath = str(imgpath)
@@ -21,15 +38,12 @@ def dice_image(imgpath, outdirpath):
         print(str(filename))
         cv.imwrite(str(filename), seg)
 
-#dice_image(
-#    chips_image_path,
-#    output_dir_path
-#  )
+#dice_image()
 
 #---------------------------------------------------------------------------------------------------
 
-def main():
-    ref  = orb.ImageWithORB(CachedCVImageLoader(chip_ref_path))
+def write_segments():
+    ref  = orb.ImageWithORB(CachedCVImageLoader(chips_ref_path))
     targ = CachedCVImageLoader(chips_image_path)
     targ.load_image()
     seg  = orb.SegmentedImage(targ.get_image(), ref.get_crop_rect())
@@ -40,4 +54,19 @@ def main():
         for line in fproj.get_bound_lines():
             print(f'  {line}')
 
-main()
+def main():
+    M = SingleFeatureMultiCrop()
+    M.set_algorithm('ORB')
+    M.set_target_image(chips_image_path)
+    M.set_reference_image(chips_ref_path)
+    return M
+
+#---------------------------------------------------------------------------------------------------
+
+if __name__ == 'main':
+    if check_paths():
+        main()
+    else:
+        print(usage)
+else:
+    pass
